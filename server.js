9fea8b6f21e649377a8143454f20a5162bdb2d84
Hossein Heidari <squanderness@gmail.com>
2020-10-10 10:30:42 +0330

    redo last commit

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static(process.env.UPLOAD_DIR))
 
-app.use('/v1', usersRoute)
-app.use('/v1', locationsRoute)
-app.use('/v1', eventsRoute)
+app.use('/api/v1', usersRoute)
+app.use('/api/v1', locationsRoute)
+app.use('/api/v1', eventsRoute)
 
 app.listen(process.env.PORT).on('listening', () => {
   console.log(`🚀 We are live on ${process.env.PORT}`)
